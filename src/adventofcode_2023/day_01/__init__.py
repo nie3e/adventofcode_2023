27f6eb0cb8cffc76c3caba9d375967b7991b3380
nie3e <adrcwiek@gmail.com>
2023-12-08 18:48:35 +0100

    Ends day 1

--- src/adventofcode_2023/day_01/__init__.py
+++ src/adventofcode_2023/day_01/__init__.py
@@ -1,19 +1,25 @@
 """https://adventofcode.com/2023/day/1"""
 from typing import Optional
+import re
+
+words = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7,
+         "8": 8, "9": 9, "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4,
+         "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
 
-def find_first_digit_in_string(in_: str) -> Optional[int]:
-    for c in in_:
-        if c.isdigit():
-            return int(c)
-    return None
+def first_last_digit_in_string(in_: str) -> Optional[tuple[int, int]]:
+    to_find = "|".join(words.keys())
+    matches = re.findall(f"(?=({to_find}))", in_)
+    if not matches:
+        return None
+    return words[matches[0]], words[matches[-1]]
 
 
 def trebuchet(input_string: str) -> int:
     input_sum = 0
     for line in input_string.splitlines():
-        input_sum += (find_first_digit_in_string(line) * 10 +
-                find_first_digit_in_string(line[::-1]))
+        first, last = first_last_digit_in_string(line)
+        input_sum += first * 10 + last
     return input_sum
 
 
--- tests/day_01/test_day_01.py
+++ tests/day_01/test_day_01.py
@@ -1,13 +1,18 @@
 import pytest
-from adventofcode_2023.day_01 import trebuchet, find_first_digit_in_string
+from adventofcode_2023.day_01 import trebuchet, first_last_digit_in_string
 
 
 @pytest.mark.parametrize(
     "test_input, expected",
-    (("1abc2", 1), ("pqr3stu8vwx", 3), ("a1b2c3d4e5f", 1), ("treb7uchet", 7))
+    (("1abc2", (1, 2)), ("pqr3stu8vwx", (3, 8)), ("a1b2c3d4e5f", (1, 5)),
+     ("treb7uchet", (7, 7)), ("two1nine", (2, 9)), ("eightwothree", (8, 3)),
+     ("abcone2threexyz", (1, 3)), ("xtwone3four", (2, 4)),
+     ("4nineeightseven2", (4, 2)), ("zoneight234", (1, 4)),
+     ("7pqrstsixteen", (7, 6)), ("4nineeight", (4, 8)),
+     ("xxhmbpvr7vvppfreightthreefivefive", (7, 5)), ("eighthree", (8, 3)))
 )
-def test_find_first_digit_in_string(test_input, expected) -> None:
-    assert find_first_digit_in_string(test_input) == expected
+def test_first_last_digit_in_string(test_input, expected) -> None:
+    assert first_last_digit_in_string(test_input) == expected
 
 
 def test_trebuchet_example() -> None:
@@ -16,3 +21,14 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"""
     assert trebuchet(in_str) == 142
+
+
+def test_trebuchet_example2() -> None:
+    in_str = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+    assert trebuchet(in_str) == 281
