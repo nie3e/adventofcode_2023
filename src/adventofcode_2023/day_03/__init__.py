7395ee74ea7c36b4dd96d1e0c9d9d9e27283537c
nie3e <adrcwiek@gmail.com>
2023-12-09 21:10:33 +0100

    Adds day 3 part 1

---
+++ src/adventofcode_2023/day_03/__init__.py
@@ -0,0 +1,76 @@
+import re
+from typing import Optional, Self
+
+
+class Symbol:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __eq__(self, other: Self) -> bool:
+        if self.x != other.x or self.y != other.y:
+            return False
+        return True
+
+
+class Number:
+    def __init__(self, value: int, x_start: int, x_end: int, y: int) -> None:
+        self.value = value
+        self.x_start = x_start
+        self.x_end = x_end
+        self.y = y
+
+    def __eq__(self, other: Self) -> bool:
+        if (self.value != other.value or self.x_start != other.x_start or
+                self.x_end != other.x_end or self.y != other.y):
+            return False
+        return True
+
+    def is_touching(self, symbol: Symbol) -> bool:
+        if abs(self.y - symbol.y) > 1:
+            return False
+        if self.x_start - 1 <= symbol.x <= self.x_end + 1:
+            return True
+        return False
+
+
+def get_numbers(line: str, line_idx: int) -> list[Number]:
+    numbers: list[Number] = []
+    matches = re.finditer(r"\d+", line)
+    for match in matches:
+        s, e = match.start(), match.end()
+        numbers.append(Number(int(line[s:e]), s, e-1, line_idx))
+    return numbers
+
+
+def get_symbols(line: str, line_idx: int) -> list[Symbol]:
+    symbols: list[Symbol] = []
+    matches = re.finditer(r"[^\.0-9]", line)
+    for match in matches:
+        symbols.append(Symbol(match.start(), line_idx))
+    return symbols
+
+
+def gear_ratios(input_string: str) -> int:
+    row_numbers = []
+    symbols = []
+    for i, line in enumerate(input_string.splitlines()):
+        row_numbers.append(get_numbers(line, i))
+        symbols.extend(get_symbols(line, i))
+
+    max_y = len(row_numbers) - 1
+    sum = 0
+    for s in symbols:
+        for i in range(min(0, s.y - 1), max(max_y, s.y + 1) + 1):
+            for number in row_numbers[i]:
+                if number.is_touching(s):
+                    sum += number.value
+
+    return sum
+
+
+if __name__ == '__main__':
+    with open("my_input", "r") as f:
+        my_input = f.read()
+
+    print(gear_ratios(my_input))
---
+++ tests/day_03/test_day_03.py
@@ -0,0 +1,69 @@
+import pytest
+
+from adventofcode_2023.day_03 import (gear_ratios, get_numbers, get_symbols,
+                                      Number, Symbol)
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("467..114..", [Number(467, 0, 2, 1), Number(114, 5, 7, 1)]),
+        ("...*......", []),
+        ("..35..633.", [Number(35, 2, 3, 1), Number(633, 6, 8, 1)]),
+        ("......#...", []),
+        ("617*......", [Number(617, 0, 2, 1)]),
+        (".....+.58.", [Number(58, 7, 8, 1)]),
+        ("..592.....", [Number(592, 2, 4, 1)]),
+        ("......755.", [Number(755, 6, 8, 1)]),
+        ("...$.*....", []),
+        (".664.598..", [Number(664, 1, 3, 1), Number(598, 5, 7, 1)]),
+    ]
+)
+def test_get_numbers(test_input, expected):
+    assert get_numbers(test_input, 1) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ("467..114..", []),
+        ("...*......", [Symbol(3, 1)]),
+        ("..35..633.", []),
+        ("......#...", [Symbol(6, 1)]),
+        ("617*......", [Symbol(3, 1)]),
+        (".....+.58.", [Symbol(5, 1)]),
+        ("..592.....", []),
+        ("......755.", []),
+        ("...$.*....", [Symbol(3, 1), Symbol(5, 1)]),
+        (".664.598..", []),
+    ]
+)
+def test_get_symbols(test_input, expected):
+    assert get_symbols(test_input, 1) == expected
+
+
+@pytest.mark.parametrize(
+    "number, sumbol, expected",
+    [
+        (Number(467, 0, 2, 1), Symbol(3, 1), True),
+        (Number(114, 5, 7, 1), Symbol(3, 1), False),
+        (Number(58, 7, 8, 5), Symbol(5, 5), False)
+    ]
+)
+def test_number_is_touching_symbol(number, sumbol, expected):
+    assert number.is_touching(sumbol) == expected
+
+
+def test_gear_ratios_example():
+    input_string = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+
+    assert gear_ratios(input_string) == 4361
